79fb59a1381c869caafa74cbd7304930d38ac060
Mambo Bryan <mambobryan@gmail.com>
2021-04-06 16:50:12 +0300

    chore(app): adds dependencies and main activity plus fragment - adds navigation, lifecycle and view model dependencies - adds parent layout - changes theme toolbar to none

---
+++ app/src/main/java/com/mambo/poetree/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mambo.poetree
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.mambo.poetree.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.navDrawerMain.setupWithNavController(navController)
+        binding.navBottomMain.setupWithNavController(navController)
+    }
+}
