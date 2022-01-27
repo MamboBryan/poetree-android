89065bdc3e1ee3c023c2f40c2be8bb00e8365daf
MamboBryan <mambobryan@gmail.com>
2022-01-27 10:04:56 +0300

    feature(profile): changes logout to button - changes dark mode toggle to alertview - removes redundant navigation methods from landing - changes on back pressed method to include landingFragment and setupFragment

--- features/profile/src/main/java/com/mambobryan/features/profile/ProfileFragment.kt
+++ features/profile/src/main/java/com/mambobryan/features/profile/ProfileFragment.kt
@@ -2,16 +2,23 @@ package com.mambobryan.features.profile
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.irozon.alertview.AlertActionStyle
+import com.irozon.alertview.AlertStyle
+import com.irozon.alertview.AlertTheme
+import com.irozon.alertview.AlertView
+import com.irozon.alertview.objects.AlertAction
+import com.mambo.core.utils.LoadingDialog
 import com.mambobryan.features.profile.databinding.FragmentProfileBinding
 import com.mambobryan.navigation.Destinations
 import com.mambobryan.navigation.extensions.getDeeplink
+import com.mambobryan.navigation.extensions.getNavOptionsPopUpToCurrent
 import com.mambobryan.navigation.extensions.navigate
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,7 +42,15 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                     ProfileViewModel.ProfileEvent.NavigateToTermsAndConditions -> navigateToTermsAndConditions()
                     ProfileViewModel.ProfileEvent.NavigateToUpdateAccount -> navigateToUpdateAccount()
                     ProfileViewModel.ProfileEvent.NavigateToUpdatePassword -> navigateToUpdatePassword()
-                    ProfileViewModel.ProfileEvent.ShowAppThemeDialog -> openAppThemeDialog()
+                    ProfileViewModel.ProfileEvent.NavigateToLanding -> navigateToLanding()
+                    ProfileViewModel.ProfileEvent.ShowDarkModeDialog -> showDarkModeDialog()
+                    ProfileViewModel.ProfileEvent.ShowLogOutDialog -> showLogoutDialog()
+                    ProfileViewModel.ProfileEvent.HideLoadingDialog -> {
+                        LoadingDialog.dismiss()
+                    }
+                    ProfileViewModel.ProfileEvent.ShowLoadingDialog -> {
+                        LoadingDialog.show(requireContext(), false)
+                    }
                 }
             }
         }
@@ -51,33 +66,50 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         tvProfilePassword.setOnClickListener { viewModel.onUpdatePasswordClicked() }
         tvProfileTerms.setOnClickListener { viewModel.onTermsAndConditionsClicked() }
         tvProfilePrivacy.setOnClickListener { viewModel.onPrivacyPolicyClicked() }
+        btnProfileLogOut.setOnClickListener { viewModel.onLogOutClicked() }
 
     }
 
-    private fun openAppThemeDialog() {
+    private fun showLogoutDialog() {
+        val alert = AlertView(
+            "Logging Out",
+            "We're going to miss you! \n Are you sure you want to logout?",
+            AlertStyle.BOTTOM_SHEET
+        )
+        alert.addAction(
+            AlertAction("Yes", AlertActionStyle.NEGATIVE) { viewModel.onLogOutConfirmed() }
+        )
+        alert.show(requireActivity() as AppCompatActivity)
+    }
+
+    private fun showDarkModeDialog() {
 
         val modes = listOf(
-            AppCompatDelegate.MODE_NIGHT_YES,
             AppCompatDelegate.MODE_NIGHT_NO,
+            AppCompatDelegate.MODE_NIGHT_YES,
             AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
         )
 
-        val mode = viewModel.mode.value
-        val titles = arrayOf("On", "Off", "Use device settings")
+        val isDark = viewModel.mode == modes[1] || viewModel.mode == modes[2]
 
-        val position = when (mode) {
-            AppCompatDelegate.MODE_NIGHT_YES -> 0
-            AppCompatDelegate.MODE_NIGHT_NO -> 1
-            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> 2
-            else -> 2
-        }
-
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle("Select Dark Mode")
-            .setSingleChoiceItems(titles, position) { p0, p1 ->
-                viewModel.onAppThemeSelected(modes[p1])
+        val alert = AlertView("Choose Theme", "", AlertStyle.BOTTOM_SHEET)
+        alert.addAction(
+            AlertAction("Light", AlertActionStyle.DEFAULT) {
+                viewModel.onDarkModeSelected(modes[0])
             }
-            .show()
+        )
+        alert.addAction(
+            AlertAction("Dark", AlertActionStyle.DEFAULT) {
+                viewModel.onDarkModeSelected(modes[1])
+            }
+        )
+        alert.addAction(
+            AlertAction("System Default", AlertActionStyle.DEFAULT) {
+                viewModel.onDarkModeSelected(modes[2])
+            }
+        )
+        alert.setTheme(if (isDark) AlertTheme.DARK else AlertTheme.LIGHT)
+        alert.show(requireActivity() as AppCompatActivity)
 
     }
 
@@ -87,6 +119,10 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private fun navigateToUpdatePassword() {
         navigate(getDeeplink(Destinations.UPDATE_PASSWORD))
+    }
+
+    private fun navigateToLanding() {
+        navigate(getDeeplink(Destinations.LANDING), getNavOptionsPopUpToCurrent())
     }
 
     private fun navigateToPrivacyPolicy() {}
--- features/profile/src/main/java/com/mambobryan/features/profile/ProfileViewModel.kt
+++ features/profile/src/main/java/com/mambobryan/features/profile/ProfileViewModel.kt
@@ -1,13 +1,15 @@
 package com.mambobryan.features.profile
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.mambo.data.preferences.UserPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,29 +17,55 @@ class ProfileViewModel @Inject constructor(
     private val preferences: UserPreferences
 ) : ViewModel() {
 
+    init {
+        runBlocking { mode = preferences.darkModeFlow.first() }
+    }
+
     private val _eventChannel = Channel<ProfileEvent>()
     val events = _eventChannel.receiveAsFlow()
 
-    val mode = preferences.darkModeFlow.asLiveData()
+    var mode: Int
 
-    fun onAppThemeClicked() = updateUi(ProfileEvent.ShowAppThemeDialog)
-    fun onAppThemeSelected(mode: Int) = viewModelScope.launch {
-        preferences.updateDarkMode(mode)
-    }
+    fun onAppThemeClicked() = updateUi(ProfileEvent.ShowDarkModeDialog)
+
+    fun onDarkModeSelected(mode: Int) = viewModelScope.launch { preferences.updateDarkMode(mode) }
+
     fun onUpdateAccountClicked() = updateUi(ProfileEvent.NavigateToUpdateAccount)
+
     fun onUpdatePasswordClicked() = updateUi(ProfileEvent.NavigateToUpdatePassword)
+
     fun onPrivacyPolicyClicked() = updateUi(ProfileEvent.NavigateToPrivacyPolicy)
+
     fun onTermsAndConditionsClicked() = updateUi(ProfileEvent.NavigateToTermsAndConditions)
+
+    fun onLogOutClicked() = updateUi(ProfileEvent.ShowLogOutDialog)
+
+    fun onLogOutConfirmed() = viewModelScope.launch {
+        updateUi(ProfileEvent.ShowLoadingDialog)
+        try {
+            preferences.logOut()
+            delay(2500)
+            updateUi(ProfileEvent.HideLoadingDialog)
+            updateUi(ProfileEvent.NavigateToLanding)
+        } catch (e: Exception) {
+            updateUi(ProfileEvent.HideLoadingDialog)
+        }
+    }
 
     private fun updateUi(event: ProfileEvent) = viewModelScope.launch {
         _eventChannel.send(event)
     }
 
-    sealed class ProfileEvent{
-        object ShowAppThemeDialog : ProfileEvent()
+    sealed class ProfileEvent {
+        object ShowLogOutDialog : ProfileEvent()
+        object ShowLoadingDialog : ProfileEvent()
+        object HideLoadingDialog : ProfileEvent()
+        object ShowDarkModeDialog : ProfileEvent()
+
+        object NavigateToLanding : ProfileEvent()
+        object NavigateToPrivacyPolicy : ProfileEvent()
         object NavigateToUpdateAccount : ProfileEvent()
         object NavigateToUpdatePassword : ProfileEvent()
-        object NavigateToPrivacyPolicy : ProfileEvent()
         object NavigateToTermsAndConditions : ProfileEvent()
     }
 
